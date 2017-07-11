c1f1271106178d39d0218439e1505ae01ea27a5d
Bartosz Krupa <krupa.bartosz@gmail.com>
2017-07-11 22:03:38 +0200

    update CallExpression tests

--- test/truthiness/expressions/CallExpression.ts
+++ test/truthiness/expressions/CallExpression.ts
@@ -11,6 +11,29 @@
 
 // should support Identifier
 {
+  function f() {
+    return 44;
+  }
+  f() === 44;
+}
+
+// should support FunctionExpression
+(function() {
+  return 44;
+})() === 44;
+
+// should support CallExpression
+{
+  function f2() {
+    return function() {
+      return 44;
+    };
+  }
+  f2()() === 44;
+}
+
+// should support ArrowFunctionExpression
+{
   let fn = () => 44;
   fn() === 44;
 }
