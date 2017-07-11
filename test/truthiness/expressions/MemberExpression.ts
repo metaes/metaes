ae770bf889a9aa2f040967886cb1324fd00e2e0b
Bartosz Krupa <krupa.bartosz@gmail.com>
2017-07-11 22:27:37 +0200

    add MemberExpression tests

---
+++ test/truthiness/expressions/MemberExpression.ts
@@ -0,0 +1,13 @@
+// should support not computed Identifier property
+{
+  let a = { b: 44 };
+  a.b === 44;
+}
+
+// should support computed Identifier and Literal property
+{
+  let a = { b: 44 };
+  let c = { d: 44 };
+  let d = 'd';
+  a['b'] === 44 && c[d] === 44;
+}
