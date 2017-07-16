eaf92893d62a115b108d1e46aab6818586161ffe
Bartosz Krupa <krupa.bartosz@gmail.com>
2017-07-16 16:36:52 +0200

    use prettier

--- lib/metaes.ts
+++ lib/metaes.ts
@@ -1,12 +1,10 @@
-import {parse} from "./parse";
-import {ErrorCallback, EvaluationConfig, LocatedError, SuccessCallback} from "./types";
-import {evaluate} from "./applyEval";
-import {ASTNode} from "./nodes/nodes";
-import {Environment} from "./environment";
+import { parse } from './parse';
+import { ErrorCallback, EvaluationConfig, LocatedError, SuccessCallback } from './types';
+import { evaluate } from './applyEval';
+import { ASTNode } from './nodes/nodes';
+import { Environment } from './environment';
 
-
-function noop(..._args) {
-}
+function noop(..._args) {}
 
 export type Message = { script: string; env: Environment };
 
@@ -15,69 +13,76 @@ export interface ScriptingContext {
 }
 
 export class MetaESContext implements ScriptingContext {
-  constructor(public environment: Environment | object = {},
-              public config: EvaluationConfig = {errorCallback: noop},
-              public c?: SuccessCallback,
-              public cerr?: ErrorCallback) {
-  }
+  constructor(
+    public environment: Environment | object = {},
+    public config: EvaluationConfig = { errorCallback: noop },
+    public c?: SuccessCallback,
+    public cerr?: ErrorCallback
+  ) {}
 
-  evaluate(input: string | Function | ASTNode, c?:SuccessCallback, cerr?:ErrorCallback): any | undefined {
+  evaluate(input: string | Function | ASTNode, c?: SuccessCallback, cerr?: ErrorCallback): any | undefined {
     return metaESEval(input, this.environment, this.config, c || this.c, cerr || this.cerr);
   }
 }
 
 export function consoleLoggingMetaESContext(environment: Environment | object = {}) {
-  return new MetaESContext(environment,
+  return new MetaESContext(
+    environment,
     {
-      interceptor: (evaluation) => {
+      interceptor: evaluation => {
         console.log(evaluation);
       },
       errorCallback: (e: LocatedError) => {
         console.log(e);
-      }
+      },
     },
     value => {
       console.log(value);
     },
-    e => console.log(e));
+    e => console.log(e)
+  );
 }
 
 let VMsCounter = 0;
 
-export function metaESEval(input: string | Function | ASTNode,
-                           environment: Environment | object = {},
-                           config: EvaluationConfig = {errorCallback: noop},
-                           c?: SuccessCallback,
-                           cerr?: ErrorCallback): any | undefined {
-  config.name = config.name || "VM" + VMsCounter++;
+export function metaESEval(
+  input: string | Function | ASTNode,
+  environment: Environment | object = {},
+  config: EvaluationConfig = { errorCallback: noop },
+  c?: SuccessCallback,
+  cerr?: ErrorCallback
+): any | undefined {
+  config.name = config.name || 'VM' + VMsCounter++;
 
   try {
-    let
-      node: ASTNode = (
-        (typeof input === 'object') ?
-          input :
-          parse(typeof input === "function" ? ("(" + input.toString() + ")") : input)) as ASTNode,
+    let node: ASTNode = (typeof input === 'object'
+        ? input
+        : parse(typeof input === 'function' ? '(' + input.toString() + ')' : input)) as ASTNode,
       env: Environment;
 
-    if ('names' in <any>environment) {
+    if ('names' in (<any>environment)) {
       env = environment as Environment;
     } else {
       env = {
         prev: undefined,
-        values: environment
+        values: environment,
       };
     }
     env.values['this'] = env.values;
 
     let value = Symbol('No value assigned');
-    evaluate(node, env, config,
+    evaluate(
+      node,
+      env,
+      config,
       val => {
         value = val;
         if (c) {
-          c({node, value: val})
+          c({ node, value: val });
         }
       },
-      error => cerr && cerr(error instanceof LocatedError ? error : new LocatedError(node, error)));
+      error => cerr && cerr(error instanceof LocatedError ? error : new LocatedError(node, error))
+    );
     return value;
   } catch (e) {
     if (cerr) {
