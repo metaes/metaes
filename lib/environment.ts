2fe5c5336cc407ebbcae99384120509f986a69eb
Bartosz Krupa <krupa.bartosz@gmail.com>
2017-07-17 23:50:00 +0200

    prettier

--- lib/environment.ts
+++ lib/environment.ts
@@ -1,43 +1,38 @@
 import { Environment } from 'metaes/environment';
-import { Continuation, ErrorContinuation, EvaluationConfig, EvaluationType, MetaESError } from "./types";
-import { Identifier } from "./nodeTypes";
-import { ASTNode } from "./nodes/nodes";
+import { Continuation, ErrorContinuation, EvaluationConfig, EvaluationType, MetaESError } from './types';
+import { Identifier } from './nodeTypes';
+import { ASTNode } from './nodes/nodes';
 
-export class EnvNotFoundError extends Error {
-}
+export class EnvNotFoundError extends Error {}
 
 export interface EnvironmentData {
   values: object;
-  references?: {[key:string]:Reference};
+  references?: { [key: string]: Reference };
 }
 
 export interface Environment extends EnvironmentData {
   prev?: Environment;
-  
+
   // At the moment used only for CatchClause.
   // Intended not to be available for client JavaScript programs
   internal?: Environment;
 }
 
-export function callInterceptor(e: ASTNode,
-                                config: EvaluationConfig,
-                                value,
-                                env: Environment,
-                                type: EvaluationType) {
-  config.interceptor && config.interceptor({
-    e,
-    value:
-      e.type === "Identifier" ? getValueOrReference((e as Identifier).name, env, config, value) : value,
-    env,
-    type,
-    timestamp: new Date().getTime()
-  });
+export function callInterceptor(e: ASTNode, config: EvaluationConfig, value, env: Environment, type: EvaluationType) {
+  config.interceptor &&
+    config.interceptor({
+      e,
+      value: e.type === 'Identifier' ? getValueOrReference((e as Identifier).name, env, config, value) : value,
+      env,
+      type,
+      timestamp: new Date().getTime(),
+    });
 }
 
 export interface Reference {
-  name:string;
-  value:any;
-  createdByMetaES:boolean;
+  name: string;
+  value: any;
+  createdByMetaES: boolean;
 }
 
 export class ReferenceCtor implements Reference {
@@ -45,48 +40,59 @@ export class ReferenceCtor implements Reference {
     return this._createdByMetaES;
   }
 
-  constructor(public name: string, public value: any,
-              public environment: Environment, private _createdByMetaES: boolean) {
-  }
+  constructor(
+    public name: string,
+    public value: any,
+    public environment: Environment,
+    private _createdByMetaES: boolean
+  ) {}
 }
 
-export function deserializeEnvironment(environment:EnvironmentData):Environment{
-  
-}
+export function deserializeEnvironment(environment: EnvironmentData): Environment {}
 
 // TODO: verify if it's really needed
-export function setValueAndCallAfterInterceptor(e: ASTNode,
-                                                env: Environment,
-                                                config: EvaluationConfig,
-                                                name: string,
-                                                value: any,
-                                                isDeclaration: boolean,
-                                                c: Continuation,
-                                                cerr: ErrorContinuation) {
-  setValue(env, name, value, isDeclaration,
+export function setValueAndCallAfterInterceptor(
+  e: ASTNode,
+  env: Environment,
+  config: EvaluationConfig,
+  name: string,
+  value: any,
+  isDeclaration: boolean,
+  c: Continuation,
+  cerr: ErrorContinuation
+) {
+  setValue(
+    env,
+    name,
+    value,
+    isDeclaration,
     value => {
-      callInterceptor(e, config, getValueOrReference(name, env, config, value), env, "exit");
+      callInterceptor(e, config, getValueOrReference(name, env, config, value), env, 'exit');
       c(value);
-    }, cerr);
+    },
+    cerr
+  );
 }
 
-export function setValue(env: Environment,
-                         name: string,
-                         value: any,
-                         isDeclaration: boolean,
-                         c: Continuation,
-                         cerr: ErrorContinuation) {
+export function setValue(
+  env: Environment,
+  name: string,
+  value: any,
+  isDeclaration: boolean,
+  c: Continuation,
+  cerr: ErrorContinuation
+) {
   let _env: Environment | undefined = env;
   if (isDeclaration) {
     setReference(env, name, value, isDeclaration);
-    c(env.values[name] = value);
+    c((env.values[name] = value));
   } else {
     while (_env) {
       // TODO: TS shouldn't complain here, should he?
-      if (name in <any>_env.values) {
+      if (name in (<any>_env.values)) {
         // TODO: set reference value as well
         setReference(env, name, value, false);
-        c(_env.values[name] = value);
+        c((_env.values[name] = value));
         return;
       }
       _env = _env.prev;
@@ -99,13 +105,15 @@ type Container = {
   env: Environment;
   name: string;
   value: any;
-}
+};
 
-function _getValue(env: Environment,
-                   name: string,
-                   returnWithContainer: boolean = false,
-                   c: (container: Container) => void,
-                   cerr: ErrorContinuation) {
+function _getValue(
+  env: Environment,
+  name: string,
+  returnWithContainer: boolean = false,
+  c: (container: Container) => void,
+  cerr: ErrorContinuation
+) {
   let _env: Environment | undefined = env;
   do {
     if (!_env) {
@@ -119,13 +127,13 @@ function _getValue(env: Environment,
       }
     }
     // TODO: TS shouldn't complain here, no?
-    if (name in <any>_env.values) {
+    if (name in (<any>_env.values)) {
       let value = _env.values[name];
 
       // return required here to avoid calling `cerr` at the end
-      return c(returnWithContainer ? {env: _env, name, value} : value);
+      return c(returnWithContainer ? { env: _env, name, value } : value);
     }
-  } while (_env = _env.prev);
+  } while ((_env = _env.prev));
 
   cerr(new ReferenceError(`"${name}" is not defined.`));
 }
@@ -144,19 +152,25 @@ function setReference(env: Environment, name: string, value: any, createdByMetaE
   return reference;
 }
 
-export function getReference(env: Environment,
-                             name: string,
-                             c: (reference: Reference) => void,
-                             cerr: ErrorContinuation) {
-  _getValue(env, name, true,
-    ({env, name, value}) => {
+export function getReference(
+  env: Environment,
+  name: string,
+  c: (reference: Reference) => void,
+  cerr: ErrorContinuation
+) {
+  _getValue(
+    env,
+    name,
+    true,
+    ({ env, name, value }) => {
       if (!env.references) {
-        env.references = {}
+        env.references = {};
       }
       c(env.references[name] || setReference(env, name, value, false));
-    }, cerr);
+    },
+    cerr
+  );
 }
-
 
 /**
  * Utility allowing to avoid CPS overhead.
@@ -164,7 +178,7 @@ export function getReference(env: Environment,
  */
 export function getReferenceNonCPS(env: Environment, name: string) {
   let result, error;
-  getReference(env, name, _result => result = _result, _error => error = _error);
+  getReference(env, name, _result => (result = _result), _error => (error = _error));
   // ignore error, because it's only for reporting
   return result;
 }
@@ -183,4 +197,3 @@ export function getValueOrReference(name: string, env: Environment, config: Eval
 export function getValue(env: Environment, name: string, c: Continuation, cerr: ErrorContinuation) {
   _getValue(env, name, false, c, cerr);
 }
-
