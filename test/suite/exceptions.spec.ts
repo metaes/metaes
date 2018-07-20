6adaf10641173bad35d863f71bdf9f4330f44813
Bartosz Krupa <krupa.bartosz@gmail.com>
2018-07-20 18:05:52 +0200

    Add tests for exiting block statements

--- test/suite/exceptions.spec.ts
+++ test/suite/exceptions.spec.ts
@@ -1,12 +1,43 @@
-
 import { describe, it } from "mocha";
-import { metaesEval } from "../../lib/metaes";
+import { evalFunctionBody, MetaesContext, metaesEval } from "../../lib/metaes";
+import { assert } from "chai";
 
 describe("Exceptions", () => {
   it("should throw ReferenceError", () =>
     new Promise((resolve, _reject) => {
-      metaesEval(`a`, null, resolve);
+      metaesEval(`a`, null, x => {
+        assert.equal(x.type, "ReferenceError");
+        resolve();
+      });
     }));
   describe("From host functions", () => {});
   describe("From MetaES functions", () => {});
+
+  describe("From blocks", () => {
+    it("should exit block statement", async () => {
+      try {
+        await evalFunctionBody(new MetaesContext(), function() {
+          throw 1;
+          return true;
+        });
+        throw new Error("Didn't throw");
+      } catch (e) {
+        assert.equal(e.type, "ThrowStatement");
+      }
+    });
+
+    it("should exit block statement when throwing from function", async () => {
+      try {
+        await evalFunctionBody(new MetaesContext(), function() {
+          (() => {
+            throw 1;
+          })();
+          return true;
+        });
+        throw new Error("Didn't throw");
+      } catch (e) {
+        assert.equal(e.type, "ThrowStatement");
+      }
+    });
+  });
 });
