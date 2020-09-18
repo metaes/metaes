5c5299c3ac7138cc46eac2546f0db6337e4952b9
Bartosz Krupa <krupa.bartosz@gmail.com>
2020-09-18 15:16:44 +0200

    move

--- test/spec/testUtils.ts
+++ test/spec/testUtils.ts
@@ -1,3 +1,13 @@
+import { assert } from "chai";
+import * as fs from "fs-extra";
+import * as glob from "glob";
+import { zip } from "lodash";
+import { before, describe, it } from "mocha";
+import * as pify from "pify";
+import { callcc } from "../../lib/callcc";
+import { getEnvironmentBy } from "../../lib/environment";
+import { presentException } from "../../lib/exceptions";
+import { ExportEnvironmentSymbol } from "../../lib/interpreter/modules";
 import { createScript } from "../../lib/metaes";
 import { Environment, Evaluate } from "../../lib/types";
 
@@ -12,3 +22,59 @@ export const evaluateHelper = (
     script.url = name;
     evalFn(script, resolve, reject, env);
   });
+
+const globalEnv = {
+  values: {
+    assert,
+    callcc,
+    getExports(_, c, cerr, env) {
+      const exportsEnv = getEnvironmentBy(env, (env) => env[ExportEnvironmentSymbol]);
+      if (exportsEnv) {
+        c(exportsEnv.values);
+      } else {
+        cerr(new Error("Couldn't find exports."));
+      }
+    }
+  },
+  prev: { values: global }
+};
+
+export function buildTests(folder: string, evalFn: Evaluate, testNamePrefix = "", logError = true) {
+  // generate tests on runtime
+  before(async () => {
+    const files = (await pify(glob)(`/${folder}/*.spec.js`)).map(async (file) => ({
+      name: file,
+      contents: (await fs.readFile(file)).toString()
+    }));
+    return (await Promise.all(files)).forEach(({ contents, name }) => {
+      const fileName = name;
+      const testNames = contents.match(/\/\/ test: [^\n]+\n/g);
+      const tests = contents.split(/\/\/ test: .+\n/).filter((line) => line.length);
+      const suiteName = name.substring(name.lastIndexOf("/") + 1);
+
+      describe(`${testNamePrefix} ${suiteName}`, () => {
+        zip(testNames, tests).forEach(([name, value]) => {
+          if (name.includes(":skip")) {
+            return;
+          }
+          const testName = `${testNamePrefix} ${name.replace("// test:", "").trim()}`;
+          it(testName, async () => {
+            try {
+              await evaluateHelper(evalFn, value, fileName, { values: {}, prev: globalEnv });
+            } catch (e) {
+              if (logError) {
+                console.log(e);
+              }
+              const message = presentException(e);
+              console.log(message);
+              throw new Error(message);
+            }
+          });
+        });
+      });
+    });
+  });
+
+  // it is a placeholder to force mocha to run `before` function
+  it("noop", () => {});
+}
