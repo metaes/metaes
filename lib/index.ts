0b55e957e192bb0af44bfc50ce0b2e665cb81520
Bartosz Krupa <krupa.bartosz@gmail.com>
2021-06-12 13:02:43 +0200

    add exporting modules

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+export * from "./metaes";
+export * from "./callcc";
+export * from "./environment";
+export * from "./interpreters";
+export * from "./metafunction";
+export * from "./parse";
+export * from "./script";
